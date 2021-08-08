85b67ab211cf7237c90e963578357d5a870e9aa4
sortsom <som.ssp2018@gmail.com>
2021-08-08 15:31:24 +0700

    edit category

---
+++ database/factories/CatagoryFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Catagory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CatagoryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Catagory::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
